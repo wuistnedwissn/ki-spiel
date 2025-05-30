652599b85b30294fff9c07dae318140490abaa30
wuistnedwissn <markushagl2@gmail.com>
2025-05-30 14:36:56 +0200

    adding TypeScript files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode
+  children: React.ReactNode;
 }) {
-	return (
-	<html lang="de">
-	<body>{children}</body>
-	</html>
-	);
+  return (
+    <html lang="de">
+      <body>{children}</body>
+    </html>
+  );
 }
